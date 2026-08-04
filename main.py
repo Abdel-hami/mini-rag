27677a1aeba928ca76d8b6f5515ce27f29193c9f
Abdelhamid El Bouni <elbouni2003@gmail.com>
2026-08-04 03:18:36 +0100

    first app

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
+from routes.base import base_router
 app = FastAPI()
 # uvicorn app:app --reload --ip 0.0.0.0 - ip forwarding to access the app from outside the container
-@app.get("/hello")
-def hello():
-    return {"message": "Hello, World!"}
+app.include_router(base_router)
