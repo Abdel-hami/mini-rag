302311c427dc9fa9b74fc9631a06505be31845be
Abdelhamid El Bouni <elbouni2003@gmail.com>
2026-08-06 23:25:13 +0100

    we mooove

---
+++ src/controllers/ProjectController.py
@@ -0,0 +1,14 @@
+
+from .BaseController import BaseController
+import os
+
+class ProjectController(BaseController):
+    def __init__(self):
+        super().__init__()
+    
+    def get_project_path(self, project_id:str):
+
+        projec_dir = os.path.join(self.files_dir, project_id)
+        if not os.path.exists(projec_dir):
+            os.makedirs(projec_dir)
+        return projec_dir
