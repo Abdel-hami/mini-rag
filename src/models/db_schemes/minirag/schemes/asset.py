280fe947515aa283a73d41350f72cef4a2f8a521
Abdelhamid El Bouni <elbouni2003@gmail.com>
2026-08-26 01:58:20 +0100

    migrating from mongo to postgres

---
+++ src/models/db_schemes/minirag/schemes/asset.py
@@ -0,0 +1,29 @@
+from .rag_base import SQLAlchemyBase
+from sqlalchemy import Column, String, Integer, DateTime, func , ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID,JSONB
+import uuid
+from sqlalchemy import Index
+
+# func is used to get the current time
+
+class Asset(SQLAlchemyBase):
+
+    __tablename__ = "assets"
+
+    asset_id = Column(Integer, primary_key=True, autoincrement=True)
+    asset_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, nullable=False, unique=True)
+
+    asset_name = Column(String, nullable=False)
+    asset_type = Column(String, nullable=False)
+    asset_size = Column(Integer, nullable=False)
+    asset_config = Column(JSONB, nullable=True)
+
+    asset_project_id = Column(Integer,ForeignKey("projects.project_id"), nullable=False)
+
+    project = relationship("Project", back_populates="assets")
+
+    __table_args__ = (
+        Index("ix_asset_project_id", asset_project_id),
+        Index("ix_asset_type", asset_type),
+    )
