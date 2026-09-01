79343f2e98822ebf8afb05fc12b87c1d06868a2c
Abdelhamid El Bouni <elbouni2003@gmail.com>
2026-09-01 23:47:52 +0100

    create pgvector

--- src/stores/vectoredb/VectorDBEnum.py
+++ src/stores/vectoredb/VectorDBEnum.py
@@ -3,7 +3,23 @@ from enum import Enum
 
 class VectorDBEnum(Enum):
     QDRANT = "qdrant"
+    PGVECTOR = "pgvector"
 
 class DistanceMethodEnum(Enum):
     COSINE = "cosine"
     DOT = "dot"
+class PgVectorTableSchemeEnums(Enum):
+    ID = "id"
+    TEXT = "text"
+    VECTOR = "vector"
+    CHUNK_ID = "chunk_id"
+    METADATA = "metadata"
+    _PREFIX = "pgvector"
+    
+class PgVectorDistanceMethodEnums(Enum):
+    COSINE = "vecor_cosine_ops"
+    DOT = "vector_12_ops"
+
+class PgVectorIndexingTypeEnums(Enum):
+    HNSW = "hnsw"
+    IVFFLAT = "ivfflat"
