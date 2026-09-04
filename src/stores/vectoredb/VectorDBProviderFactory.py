36d234e84e711bbcfe5de9a9e91d6ad1fcfd3e09
Abdelhamid El Bouni <elbouni2003@gmail.com>
2026-09-04 19:15:14 +0100

    update vectordbfactory provider

--- src/stores/vectoredb/VectorDBProviderFactory.py
+++ src/stores/vectoredb/VectorDBProviderFactory.py
@@ -1,12 +1,18 @@
-from  .providers.QdrantDB import QdrantDB
+from  .providers import QdrantDB, PGVectorDB
 from .VectorDBEnum import VectorDBEnum
 from controllers.BaseController import BaseController
+from sqlalchemy import sessionmaker
+
+
 class VectorDBProviderFactory():
-    def __init__(self, config: dict):
+    def __init__(self, config: dict, db_client: sessionmaker=None):
         self.config = config
         self.base_controller = BaseController()
+        self.db_client = db_client
 
     def create(self, provider: str):
         if provider == VectorDBEnum.QDRANT.value:
-            return QdrantDB(db_path=self.base_controller.get_db_path(self.config.VECTOR_DB_PATH), distance_method=self.config.VECTOR_DB_DISTANCE_METHOD)
+            return QdrantDB(db_client=self.base_controller.get_db_path(self.config.VECTOR_DB_PATH), distance_method=self.config.VECTOR_DB_DISTANCE_METHOD)
+        if provider == VectorDBEnum.PGVECTOR.value:
+            return PGVectorDB(db_client=self.db_client, distance_method=self.config.VECTOR_DB_DISTANCE_METHOD, default_vector_size=self.config.VECTOR_DB_DEFAULT_VECTOR_SIZE, default_index_threshold=self.config.DEFAULT_PGVECTOR_INDEX_THRESHOLD)
         return None
