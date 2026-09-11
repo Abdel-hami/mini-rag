1dacd9f19e5a2eb4913f30ca87a46316213bbde1
Abdelhamid El Bouni <elbouni2003@gmail.com>
2026-09-11 23:52:40 +0100

    fix docker ports errors

--- docker/rag/entrypoint.sh
+++ docker/rag/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 set -e 
-
+# run database migrations
 echo "runing databse migrations..."
-cd /app/models/db_schemes/minirag
+cd /app/models/db_schemes/minirag/
 alembic upgrade head
 cd /app
+# start the application
+echo "starting the application..."
+exec "$@"
+
